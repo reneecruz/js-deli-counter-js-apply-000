5546cf94845ee9ff4e90c1c957f6b6a760920614
Renée Cruz <reneebasscruz@gmail.com>
2019-03-06 21:30:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-function takeANumber(katzDeliLine, name) {
+function takeANumber(katzDeliLine, number) {
   katzDeliLine = katzDeliLine.push(name);
   return (`Welcome, ${name}. You are number ${katzDeliLine} in line.`);
 }
